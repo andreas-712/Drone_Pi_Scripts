d5bf525b292ae05e3f4bdf344e46080f895c2342
Andreas Mendez-Cadrin <amendezcadrin@gmail.com>
2025-08-09 16:43:33 -0700

    Added Python test script for controller, and minor comment fixes

--- joystick_test.py
+++ joystick_test.py
@@ -1,13 +1,16 @@
 """
-This script is for receiving data from the Xbox controller
-Joystick values must be scaled 0-20000 for DShot
+This script is for testing the script and Xbox controller
+Joystick values must be scaled 0-20,000
 Left stick for 2D planar motion, right stick for elevation
 """
 
+"""
+Important note: side-to-side, forward-back, vertical will be at 10,000 when controller is at rest
+We must make sure this is accounted for in STM code before being taken at absolute value in the STM
+"""
+
 import time
-import struct
 import sys
-import spidev # spidev will only install on Linux host, like the Pi
 import pygame as p
 
 # Scale the -1 to 1 float (must fit within 2 bytes unsigned int)
@@ -15,21 +18,6 @@ NUM_RESOLUTION = 10000
 
 p.init()
 p.joystick.init()
-
-spi = spidev.SpiDev()
-# Pi's bus 0, CS 0 (GPIO8 / pin 24 on Pi)
-spi.open(0, 0)
-# Initialize SPI at 500KHz
-spi.max_speed_hz = 500_000
-# Idle low
-spi.mode = 0
-
-# Format data for SPI transfer
-def send_spi(x, y, z):
-    # Most significant byte first, with 3 signed ints
-    data = struct.pack(">hhh", x, y, z)
-    # Sends 4 values of bytes as a list
-    spi.xfer2(list(data))
 
 # If no controls are active
 if p.joystick.get_count() == 0:
@@ -57,11 +45,6 @@ try:
         p.event.pump()
 
         try:
-            """
-            Extra caution to not send negative ints,
-            though ints over the limit would be handled in STM code 
-            and will send over SPI without issue.
-            """
 
             # Side-to-side motion -- unsigned int -- left stick
             side = int(max((js.get_axis(0) + 1) * NUM_RESOLUTION, 0))
@@ -70,7 +53,7 @@ try:
             # Vertical motion -- unsigned int -- right stick
             vertical = int(max((js.get_axis(4) + 1) * NUM_RESOLUTION, 0))
 
-            send_spi(side, front, vertical)
+            print(f"Side (x): {side}, Forward (y): {front}, Vertical (z): {vertical}")
 
             error_counter = 0
 
